547ffb719d4b4c72a752ce82f5cc40f628569188
kunj patel <kunjladani5865@gmail.com>
2026-05-25 22:01:01 +0530

    Ready for Railway deploy

--- supertechmobila/wsgi.py
+++ supertechmobila/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for supertechmobila project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = '/home/kunjladani22/supertech_mobila57462'
+
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'PROJECT_NAME.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'supertechmobila.settings')
-
 application = get_wsgi_application()
